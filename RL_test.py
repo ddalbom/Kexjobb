3d7c296b04566b6a810b87deed9fe7c66199364b
petteer <47141730+petteer@users.noreply.github.com>
2019-01-29 19:30:54 +0100

    Update RL_test.py

--- RL_test.py
+++ RL_test.py
@@ -1,68 +1,70 @@
 # Reinforcement learning
 import numpy as np
 import random as rnd
+import tensorflow as tf
 
-state = np.zeros((4, 4), dtype = int)
-ACTIONS = ['right','left','up','down'] # available actions
+grid_size = 3
+domain = np.zeros((grid_size, grid_size), dtype = int)
 
+ACTIONS = ['Right', 'Left', 'Up', 'Down']
 
 # Does not use RL at the moment
+
 class Robot:
     """Implements test robot for RL algorithm. """
     def __init__(self, xpos = 0, ypos = 0):
         self.x = xpos
         self.y = ypos
-
+        # self.carry ... # Maybe = True if it carries load?
+        # self.vx/vy # Velocity maybe?
 
 def move_robot(robot):
     """Moves the robot according to the given action."""
-    global state
+    global domain
     x = robot.x
     y = robot.y
-    action = choose_action(state)
-    if action == 'right':
-        if state[0][3] == 1 or state [1][3] == 1 or state[2][3] == 1 or state [3][3] == 1: # Check if we are at the right side of the matrix
+    domain[x][y] = 0
+    p = [0.25, 0.25, 0.25, 0.25]
+    action = choose_action(domain, p)
+    if action == 'Right':
+        if x + 1 >= grid_size:
             pass
         else:
-            state[x][y] = 0
-            y += 1
-    elif action == 'left':
-        if state[0][0] == 1 or state [1][0] == 1 or state[2][0] == 1 or state [3][0] == 1: # Checks if we are at the left side of matrix
-            pass
-        else:
-            state[x][y] = 0
-            y = y - 1
-    elif action == 'up':
-        if state[0][0] == 1 or state [0][1] == 1 or state[0][2] == 1 or state [0][3] == 1: # Checks if we are at the top of the matrix
-            pass
-        else:
-            state[x][y] = 0
-            x = x - 1
-    elif action == 'down':
-        if state[3][0] == 1 or state [3][1] == 1 or state[3][2] == 1 or state [3][3] == 1: # Checks if we are at the bottom of the matrix
-            pass
-        else:
-            state[x][y] = 0
             x += 1
+    elif action == 'Left':
+        if x - 1 < 0:
+            pass
+        else:
+            x -= 1
+    elif action == 'Up':
+        if y + 1 >= grid_size:
+            pass
+        else:
+            y += 1
+    elif action == 'Down':
+        if y - 1 < 0:
+            pass
+        else:
+            y -= 1
+    x = x % grid_size
+    y = y % grid_size
     robot.x = x
     robot.y = y
-    state[x][y] = 1
+    domain[x][y] = 1
 
-def choose_action(state): # Given a state, what is the probability of performing action a?
+def choose_action(state, prob): # Given a state, what is the probability of performing action a?
     """Defines policy to follow."""
     if state[-1][-1] != 1: # Robot is not at end point
-        action = np.random.choice(ACTIONS) # Choose a random action
+        action = np.random.choice(ACTIONS, p = prob) # Choose an action at random
     return action
 
-r1 = Robot()
-state[0][0] = 1 # Our initial state
-count = 0
-print(state)
+r1 = Robot() # Create a test robot
+count = 0 # Counts the number of steps taken
 
-while state[-1][-1] != 1:
-    move_robot(r1) # Moves the robot randomly one step
-    print(state)  # Prints out the current state of the system
-    print()
-    count += 1  # Counts how many steps is needed
+while domain[2][2] != 1: # Unless
+    move_robot(r1)
+    print(domain)
+    print([r1.x, r1.y])
+    count += 1
 
-print('number of steps: ',count)
+print('Count = ', count)
